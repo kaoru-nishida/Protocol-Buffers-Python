f3722ab7a24a7359b3c2b00a557df5eb20c534bd
かおる <31959526+kaoru-nishida@users.noreply.github.com>
2017-12-25 21:16:13 +0900

    初回追加

---
+++ protoc.sh
@@ -0,0 +1,4 @@
+echo "this is easy proto compiler for python"
+echo "made by kaoru"
+echo ""
+protoc -I=/home/kaoru/Documents/Protocol-Buffers-Python/ --python_out=/home/kaoru/Documents/Protocol-Buffers-Python/output/ /home/kaoru/Documents/Protocol-Buffers-Python/addressbook.proto
---
+++ write.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+# coding: UTF-8
+import addressbook_pb2
+import sys
+
+# This function fills in a Person message based on user input.
+def PromptForAddress(person):
+  person.id = int(raw_input("IDを入力 : "))
+  person.name = raw_input("名前を入力 : ")
+
+  email = raw_input("メールアドレスを入力 (空でも良い): ")
+  if email != "":
+    person.email = email
+
+  while True:
+    number = raw_input("電話番号を入力 : ")
+    if number == "":
+      break
+
+    phone_number = person.phones.add()
+    phone_number.number = number
+
+    type = raw_input("これはmobile, home, or workどの端末ですか？ ")
+    if type == "mobile":
+      phone_number.type = addressbook_pb2.Person.MOBILE
+    elif type == "home":
+      phone_number.type = addressbook_pb2.Person.HOME
+    elif type == "work":
+      phone_number.type = addressbook_pb2.Person.WORK
+    else:
+      print "不明なタイプです、デフォルト値を使用します"
+
+# Main procedure:  Reads the entire address book from a file,
+#   adds one person based on user input, then writes it back out to the same
+#   file.
+if len(sys.argv) != 2:
+  print "使用方法 :", sys.argv[0], "ADDRESS_BOOK_FILE"
+  sys.exit(-1)
+
+address_book = addressbook_pb2.AddressBook()
+
+# Read the existing address book.
+try:
+  f = open(sys.argv[1], "rb")
+  address_book.ParseFromString(f.read())
+  f.close()
+except IOError:
+  print sys.argv[1] + ": Could not open file.  Creating a new one."
+
+# Add an address.
+PromptForAddress(address_book.people.add())
+
+# Write the new address book back to disk.
+f = open(sys.argv[1], "wb")
+f.write(address_book.SerializeToString())
+f.close()
